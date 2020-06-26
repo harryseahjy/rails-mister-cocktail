45cd00b133204a30425d69e5b53956270df35900
Harold Seah <harryseahjy@gmail.com>
2020-06-26 15:33:49 +0800

    Seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,19 +16,19 @@ require 'open-uri'
 #     ingredient.save!
 # end
 
-# 9.times do
-#   data = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/random.php').read)['drinks'][0]
-#   cocktail = Cocktail.create(name: data['strDrink'].downcase)
+18.times do
+  data = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/random.php').read)['drinks'][0]
+  cocktail = Cocktail.create(name: data['strDrink'].downcase)
 
-#   cocktail.photo.attach(io: open(data['strDrinkThumb']), filename: "#{data['strDrink']}.jpg", content_type: 'image/png')
-#   ingredients = []
-#   ingredient_names = data.select { |key, _| key.to_s.match(/strIngredient/) }.values.compact
-#   ingredient_names.each do |name|
-#     result = Ingredient.find_by name: name.downcase
-#     result = Ingredient.create!(name: name.downcase) if result.nil?
-#     ingredients << result
-#   end
-#   amounts = data.select { |key, _| key.to_s.match(/strMeasure/) }.values.compact
-#   doses = amounts.zip(ingredients)
-#   doses.each { |dose| Dose.create(cocktail: cocktail, ingredient: dose[1], description: dose[0]) }
-# end
+  cocktail.photo.attach(io: open(data['strDrinkThumb']), filename: "#{data['strDrink']}.jpg", content_type: 'image/png')
+  ingredients = []
+  ingredient_names = data.select { |key, _| key.to_s.match(/strIngredient/) }.values.compact
+  ingredient_names.each do |name|
+    result = Ingredient.find_by name: name.downcase
+    result = Ingredient.create!(name: name.downcase) if result.nil?
+    ingredients << result
+  end
+  amounts = data.select { |key, _| key.to_s.match(/strMeasure/) }.values.compact
+  doses = amounts.zip(ingredients)
+  doses.each { |dose| Dose.create(cocktail: cocktail, ingredient: dose[1], description: dose[0]) }
+end
